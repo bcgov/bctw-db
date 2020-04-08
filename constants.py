62cbce3e56d06dcf390f2315cd3f17b99da395e3
Parratt <Paul.Parratt@quartech.com>
2020-04-07 20:48:56 -0700

    Deleted some files.

--- constants.py
+++
@@ -1,3 +0,0 @@
-X_WWW_HEADERS = {'Content-Type': 'application/x-www-form-urlencoded'}
-LOTEX_URL = 'https://webservice.lotek.com/API'
-VECTRONICS_URL = 'https://api.vectronic-wildlife.com/v2/collar'
