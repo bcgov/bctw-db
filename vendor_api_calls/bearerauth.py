1432f648d62f27fadaaffed8cd759c569966f63a
Parratt <Paul.Parratt@quartech.com>
2020-04-07 20:42:54 -0700

    Moved Python files to folder.

---
+++ vendor_api_calls/bearerauth.py
@@ -0,0 +1,11 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+class BearerAuth(requests.auth.AuthBase):
+    def __init__(self, token):
+        self.token = token
+
+    def __call__(self, r):
+        r.headers["authorization"] = "Bearer " + self.token
+        return r
